bcddbc6d1e48d801dc89ce42796ce7b70c76eb5f
IP <irin.potapova@gmail.com>
2017-04-12 12:33:54 -0400

    Add head injection support, both client and server, via Helmet library.

--- src/shared/component/page/not-found.jsx
+++ src/shared/component/page/not-found.jsx
@@ -1,7 +1,19 @@
 // @flow
 
 import React from 'react'
+import Helmet from 'react-helmet'
 
-const NotFoundPage = () => <p>Page not found</p>
+const title = 'Page Not Found'
+
+const NotFoundPage = () =>
+  <div>
+    <Helmet
+      title={title}
+      meta={[
+        { name: 'description', content: 'A page to say hello' },
+        { property: 'og:title', content: title },
+      ]}
+    />
+  </div>
 
 export default NotFoundPage
